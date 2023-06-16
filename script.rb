4743e9695c7c0e7ccc565ce4f3875ace6687a602
ChrisClevenger <aenguscoen@gmail.com>
2023-06-16 13:40:43 -0500

    Code comparison feature for pair

--- script.rb
+++ script.rb
@@ -5,9 +5,15 @@ def bubble_sort(array)
 ## Create mehtod for each consecutive pair that reverses index is left > right
     sorted_array = []
 
-    array.each_cons (2) do | pair | 
-        puts pair.inspect 
+    array.each_cons (2) do | x, y | 
+        pair = [x, y]
+        if x > y
+        puts pair.reverse.inspect
+        else
+            puts pair.inspect
     end
+end
+
 end
 
 bubble_sort([4,3,78,2,0,2])
