da4a0be5c54398e60d1af06453abf986f30a7c3c
ChrisClevenger <aenguscoen@gmail.com>
2023-06-16 13:16:35 -0500

    Code function to pair integers in array

---
+++ script.rb
@@ -0,0 +1,19 @@
+## Create method to hold array
+
+def bubble_sort(array)
+
+## Create mehtod for each consecutive pair that reverses index is left > right
+    sorted_array = []
+
+    array.each_cons (2) do | pair | 
+        puts pair.inspect 
+    end
+end
+
+bubble_sort([4,3,78,2,0,2])
+
+            
+
+## Iterate through function array.length - 1 times 
+
+## Puts resulting array 
